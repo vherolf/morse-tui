8b51370934226dea1477b16d586f1cc059f3d71f
vherolf <vherolf@proclos.com>
2023-05-30 14:37:10 +0200

    changed to middle dot for output

--- morse-tui.py
+++ morse-tui.py
@@ -9,6 +9,9 @@ from textual.widgets import Input
 from textual.widgets import Footer
 from textual.binding import Binding
 
+dot = '\N{Middle Dot}'
+dash = '\N{Hyphen-Minus}'
+
 class Morse(pydantic.BaseModel):
     letter: str
     sequence: str
@@ -17,91 +20,91 @@ class Morse(pydantic.BaseModel):
         super(Morse, self).__init__(letter=letter, sequence=sequence)
 
 morsecode = (
-        Morse(" "," "),
-        Morse("A",".-"),
-        Morse("B","-..."),
-        Morse("C","-.-."),
-        Morse("D","-.."),
-        Morse("E","."),
-        Morse("F","..-."),
-        Morse("G","--."),
-        Morse("H","...."),
-        Morse("I",".."),
-        Morse("J",".---"),
-        Morse("K","-.-"),
-        Morse("L",".-.."),
-        Morse("M","--"),
-        Morse("N","-."),
-        Morse("O","---"),
-        Morse("P",".--."),
-        Morse("Q","--.-"),
-        Morse("R",".-."),
-        Morse("S","..."),
-        Morse("T","-"),
-        Morse("U","..-"),
-        Morse("V","...-"),
-        Morse("W",".--"),
-        Morse("X","-..-"),
-        Morse("Y","-.--"),
-        Morse("Z","--.."),
-        Morse("1",".----"),
-        Morse("2","..---"),
-        Morse("3","...--"),
-        Morse("4","....-"),
-        Morse("5","....."),
-        Morse("6","-...."),
-        Morse("7","--..."),
-        Morse("8","---.."),
-        Morse("9","----."),
-        Morse("0","-----"),
-        Morse("À",".--.-"),
-        Morse("Ä",".-.-"),
-        Morse("È",".-..-"),
-        Morse("É","..-.."),
-        Morse("Ö","---."),
-        Morse("Ü","..--"),
-        Morse("ß","...--.."),
-        Morse("CH","----"),
-        Morse("Ñ","--.--"),
-        Morse(".",".-.-.-"),
-        Morse(",","--..--"),
-        Morse(":","---..."),
-        Morse(";","-.-.-."),
-        Morse("?","..--.."),
-        Morse("-","-....-"),
-        Morse("_","..--.-"),
-        Morse("(","-.--."),
-        Morse(")","-.--.-"),
-        Morse("'",".----."),
-        Morse("=","-...-"),
-        Morse("+",".-.-."),
-        Morse("/","-..-."),
-        Morse("@",".--.-."),
-        Morse("KA","-.-.-"),
-        Morse("BT","-...-"),
-        Morse("AR",".-.-."),
-        Morse("VE","...-."),
-        Morse("SK","...-.-"),
-        Morse("SOS","...---..."),
-        Morse("HH","........"),
-        Morse("!","-.-.--"),
+        Morse(" ",f" "),
+        Morse("A",f"{dot}{dash}"),
+        Morse("B",f"{dash}{dot}{dot}{dot}"),
+        Morse("C",f"{dash}{dot}{dash}{dot}"),
+        Morse("D",f"{dash}{dot}{dot}"),
+        Morse("E",f"{dot}"),
+        Morse("F",f"{dot}{dot}{dash}{dot}"),
+        Morse("G",f"{dash}{dash}{dot}"),
+        Morse("H",f"{dot}{dot}{dot}{dot}"),
+        Morse("I",f"{dot}{dot}"),
+        Morse("J",f"{dot}{dash}{dash}{dash}"),
+        Morse("K",f"{dash}{dot}{dash}"),
+        Morse("L",f"{dot}{dash}{dot}{dot}"),
+        Morse("M",f"{dash}{dash}"),
+        Morse("N",f"{dash}{dot}"),
+        Morse("O",f"{dash}{dash}{dash}"),
+        Morse("P",f"{dot}{dash}{dash}{dot}"),
+        Morse("Q",f"{dash}{dash}{dot}{dash}"),
+        Morse("R",f"{dot}{dash}{dot}"),
+        Morse("S",f"{dot}{dot}{dot}"),
+        Morse("T",f"{dash}"),
+        Morse("U",f"{dot}{dot}{dash}"),
+        Morse("V",f"{dot}{dot}{dot}{dash}"),
+        Morse("W",f"{dot}{dash}{dash}"),
+        Morse("X",f"{dash}{dot}{dot}{dash}"),
+        Morse("Y",f"{dash}{dot}{dash}{dash}"),
+        Morse("Z",f"{dash}{dash}{dot}{dot}"),
+        Morse("1",f"{dot}{dash}{dash}{dash}{dash}"),
+        Morse("2",f"{dot}{dot}{dash}{dash}{dash}"),
+        Morse("3",f"{dot}{dot}{dot}{dash}{dash}"),
+        Morse("4",f"{dot}{dot}{dot}{dot}{dash}"),
+        Morse("5",f"{dot}{dot}{dot}{dot}{dot}"),
+        Morse("6",f"{dash}{dot}{dot}{dot}{dot}"),
+        Morse("7",f"{dash}{dash}{dot}{dot}{dot}"),
+        Morse("8",f"{dash}{dash}{dash}{dot}{dot}"),
+        Morse("9",f"{dash}{dash}{dash}{dash}{dot}"),
+        Morse("0",f"{dash}{dash}{dash}{dash}{dash}"),
+        Morse("À",f"{dot}{dash}{dash}{dot}{dash}"),
+        Morse("Ä",f"{dot}{dash}{dot}{dash}"),
+        Morse("È",f"{dot}{dash}{dot}{dot}{dash}"),
+        Morse("É",f"{dot}{dot}{dash}{dot}{dot}"),
+        Morse("Ö",f"{dash}{dash}{dash}{dot}"),
+        Morse("Ü",f"{dot}{dot}{dash}{dash}"),
+        Morse("ß",f"{dot}{dot}{dot}{dash}{dash}{dot}{dot}"),
+        Morse("CH",f"{dash}{dash}{dash}{dash}"),
+        Morse("Ñ",f"{dash}{dash}{dot}{dash}{dash}"),
+        Morse(".",f"{dot}{dash}{dot}{dash}{dot}{dash}"),
+        Morse(",",f"{dash}{dash}{dot}{dot}{dash}{dash}"),
+        Morse(":",f"{dash}{dash}{dash}{dot}{dot}{dot}"),
+        Morse(";",f"{dash}{dot}{dash}{dot}{dash}{dot}"),
+        Morse("?",f"{dot}{dot}{dash}{dash}{dot}{dot}"),
+        Morse("-",f"{dash}{dot}{dot}{dot}{dot}{dash}"),
+        Morse("_",f"{dot}{dot}{dash}{dash}{dot}{dash}"),
+        Morse("(",f"{dash}{dot}{dash}{dash}{dot}"),
+        Morse(")",f"{dash}{dot}{dash}{dash}{dot}{dash}"),
+        Morse("'",f"{dot}{dash}{dash}{dash}{dash}{dot}"),
+        Morse("=",f"{dash}{dot}{dot}{dot}{dash}"),
+        Morse("+",f"{dot}{dash}{dot}{dash}{dot}"),
+        Morse("/",f"{dash}{dot}{dot}{dash}{dot}"),
+        Morse("@",f"{dot}{dash}{dash}{dot}{dash}{dot}"),
+        Morse("KA",f"{dash}{dot}{dash}{dot}{dash}"),
+        Morse("BT",f"{dash}{dot}{dot}{dot}{dash}"),
+        Morse("AR",f"{dot}{dash}{dot}{dash}{dot}"),
+        Morse("VE",f"{dot}{dot}{dot}{dash}{dot}"),
+        Morse("SK",f"{dot}{dot}{dot}{dash}{dot}{dash}"),
+        Morse("SOS",f"{dot}{dot}{dot}{dash}{dash}{dash}{dot}{dot}{dot}"),
+        Morse("HH",f"{dot}{dot}{dot}{dot}{dot}{dot}{dot}{dot}"),
+        Morse("!",f"{dash}{dot}{dash}{dot}{dash}{dash}"),
         )
 
 ## decode helper functions
 
-def sentencesFromMorseText(text='... --- \t .  \n ... --- .  \r  ... --- .'):
+def sentencesFromMorseText(text=f'{dot}{dot}{dot} {dash}{dash}{dash} \t {dot}  \n {dot}{dot}{dot} {dash}{dash}{dash} {dot}  \r  {dot}{dot}{dot} {dash}{dash}{dash} {dot}'):
     ## splits text into sentences
     sentences = re.split(r'(\\{1,}|\r{1,}|\n{1,})', text)
     #print('sentences', sentences)
     return sentences
 
-def wordsFromMorseSentences(sentence='... --- .  ... --- .   ... --- .'):
+def wordsFromMorseSentences(sentence=f'{dot}{dot}{dot} {dash}{dash}{dash} {dot}  {dot}{dot}{dot} {dash}{dash}{dash} {dot}   {dot}{dot}{dot} {dash}{dash}{dash} {dot}'):
     ## splits a sentence into words when two or more spaces 
     words = re.split("\s{2,}", sentence)
     #print('words', words)
     return words
 
-def charsFromMorseWords(word='... --- .  ... --- .   ... --- .'):
+def charsFromMorseWords(word=f'{dot}{dot}{dot} {dash}{dash}{dash} {dot}  {dot}{dot}{dot} {dash}{dash}{dash} {dot}   {dot}{dot}{dot} {dash}{dash}{dash} {dot}'):
     ## splits a sentence into words when two or more spaces 
     chars = re.split("\s{1,}", word)
     #print('chars', chars)
@@ -109,9 +112,12 @@ def charsFromMorseWords(word='... --- .  ... --- .   ... --- .'):
 
 def normalize(text='_'):
     # replace unusual minus and and not needed white spaces
-    return text.replace('_','-')
+    return text.replace('_','{dash}')
 
-def decodeChar(char='.'):
+def toDotDash(text=""):
+    return text.replace('-',f'{dash}').replace('.',f'{dot}')
+
+def decodeChar(char=f'{dot}'):
     for glyph in morsecode:
         if glyph.sequence == char:
             return glyph.letter
@@ -136,6 +142,7 @@ def isMorseCode(text):
     
 def decodeText(text):
     result = ""
+    text = toDotDash(text)
     sentences = sentencesFromMorseText(text)
     for sentence in sentences:
         words = wordsFromMorseSentences(sentence)
